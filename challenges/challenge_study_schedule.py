cd24c2cd81848af39f1759c9f4e430f76b19b281
martins <gabrielamspinola@gmail.com>
2023-03-08 19:29:46 -0300

    [fix] req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == "" or target_time.isnumeric() is False:
+    if target_time == " " or target_time.isnumeric() is False:
         return None
 
     counter = 0
+    num = int(target_time)
     for period in permanence_period:
-        if period[0] == period or period[1] == period:
+        if period[0] == num or period[1] == num:
             counter += 1
-        elif period[0] < target_time and period[1] > target_time:
+        elif period[0] < num and period[1] > num:
             counter += 1
 
     return counter
