490b682f2fc5fe0ee21c1d45fde3934959d4bc08
martins <gabrielamspinola@gmail.com>
2023-03-08 19:51:53 -0300

    [feat] req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("livros", 1) == "l_sorvi"
+    assert encrypt_message("livros", 2) == "rvil_so"
+    assert encrypt_message("livros", 10) == "sorvil"
+
+    with pytest.raises(TypeError):
+        encrypt_message(8, 8)
+    with pytest.raises(TypeError):
+        encrypt_message("string", "string")
