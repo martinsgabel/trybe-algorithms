e094106e45cf17f9969f943014b54774cf975b8d
martins <gabrielamspinola@gmail.com>
2023-03-08 19:21:24 -0300

    iniciando projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time == "" or target_time.isnumeric() is False:
+        return None
+
+    counter = 0
+    for period in permanence_period:
+        if period[0] == period or period[1] == period:
+            counter += 1
+        elif period[0] < target_time and period[1] > target_time:
+            counter += 1
+
+    return counter
+
     raise NotImplementedError
+
+
+# retorno contador
+# -vv (mais detalhes do erro)
