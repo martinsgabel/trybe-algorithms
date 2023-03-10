af91addd7cdaf35dd72ff015d7770d184c2b2498
martins <gabrielamspinola@gmail.com>
2023-03-10 17:36:30 -0300

    [feat] req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not nums
+        or isinstance(nums, str)
+        or len(nums) < 2
+        or any(n < 0 for n in nums.sort())
+    ):
+        return False
+
+    nums.sort()
+    for n in range(len(nums) - 1):
+        if nums[n] == nums[n + 1]:
+            return nums[n]
