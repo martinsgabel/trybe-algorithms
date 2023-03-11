fb5dec900a43b4d59789642e995ed6d1cc87a398
martins <gabrielamspinola@gmail.com>
2023-03-11 19:23:20 -0300

    [fix] req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,26 @@
-def is_palindrome_recursive(word, low_index=0, high_index=0):
-    if not word:
-        return False
-
-    if len(word) < 1:
+def reversing_word(word):
+    if len(word) == 1:
         return word
     else:
-        return is_palindrome_recursive(word[1:]) + [word[0]]
+        return reversing_word(word[1:]) + [word[0]]
+
+
+def is_palindrome_recursive(word, low_index=0, high_index=0):
+    if (
+        not word
+        or len(word) == 0
+        or type(word) != str
+    ):
+        return False
+
+    initial_word = word
+
+    if len(word) == 1:
+        return True
+
+    final_word = "".join(reversing_word(list(word)))
+
+    if final_word == initial_word:
+        return True
+    else:
+        return False
