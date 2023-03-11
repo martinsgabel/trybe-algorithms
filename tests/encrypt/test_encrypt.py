e446ff16895591270e102eba8ec1cf91f057932c
martins <gabrielamspinola@gmail.com>
2023-03-11 19:43:30 -0300

    [fix] req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,7 +4,7 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     assert encrypt_message("livros", 1) == "l_sorvi"
-    assert encrypt_message("livros", 2) == "rvil_so"
+    assert encrypt_message("livros", 2) == "sorv_il"
     assert encrypt_message("livros", 10) == "sorvil"
 
     with pytest.raises(TypeError):
